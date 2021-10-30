c930ea3cb1cfcae1c2f40c1c80ca4079583155bd
westshae <shaewest02@gmail.com>
2021-10-30 20:26:17 +1300

    Made hud drawing modular

--- src/components/hud/hud.ts
+++ src/components/hud/hud.ts
@@ -4,33 +4,54 @@ import { app } from "../..";
 
 class HUD {
   container:Container;
+  bar:Graphics;
+  button:Graphics;
 
   constructor(){
     this.container = new Container();
+    this.bar = new Graphics();
+    this.button = new Graphics();
     this.init();
   }
 
   init(){
     app.stage.addChild(this.container);
 
-    let bar = new Graphics();
-    this.draw(bar);
+    this.draw();
     window.addEventListener('resize', ()=>{
-      this.draw(bar);
+      this.draw();
     })
 
-    this.container.addChild(bar);
+    this.container.addChild(this.bar);
+    this.container.addChild(this.button);
   }
 
-  draw(bar:Graphics){
-    let height = app.renderer.height/16;
+  draw(){
     let width = app.renderer.width;
-    bar.clear();
-    bar.beginFill(0x434343);
-    bar.drawRect(0,0,width, height);
-    bar.beginFill(0x900000);
-    bar.drawStar(height/2,height/2,5,height/2);
+    let height = app.renderer.height/16;
+
+    this.drawButton(height);
+    this.drawBar(width, height);
+
+    this.container.removeChildren();
+    
   }
+
+  drawBar(width:number, height:number){
+    this.bar.beginFill(0x434343);
+    this.bar.drawRect(0,0,width, height);
+  }
+
+  drawButton(height:number){
+    this.button.beginFill(0x900000);
+    this.button.drawStar(height/2,height/2,5,height/2);
+    this.button.interactive = true;
+    this.button.on("mousedown", () => alert("ree"));
+
+    this.button.zIndex = 100;
+  }
+
+
 }
 
 export{
