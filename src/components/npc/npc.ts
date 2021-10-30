c272ee2a35ba95311e4802c6b8c40360507e3e63
westshae <shaewest02@gmail.com>
2021-10-30 18:35:05 +1300

    created npc class, basic stats, basic constructor

---
+++ src/components/npc/npc.ts
@@ -0,0 +1,20 @@
+
+class NPC {
+  id:number;
+  type:number;//0 = Villager, 1 = Monster, 2 = Animal, 3 = Boss
+  health:number;
+  attack:number;
+  defense:number;
+
+  constructor(npcID:number){
+    switch(npcID){
+      case 1:
+        
+    }
+  }
+
+}
+
+export{
+  NPC,
+}
