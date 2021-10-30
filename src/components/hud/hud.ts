4a942a5f3bb73afe50803264a459dc7f1a9923a8
westshae <shaewest02@gmail.com>
2021-10-30 20:30:26 +1300

    Comment/refactor hud.ts

--- src/components/hud/hud.ts
+++ src/components/hud/hud.ts
@@ -14,44 +14,46 @@ class HUD {
     this.init();
   }
 
-  init(){
+  init(){//Added HUD to stage, added event for resizing, draws HUD
     app.stage.addChild(this.container);
 
+    //Draws, then adds event for detecting resize
     this.draw();
     window.addEventListener('resize', ()=>{
       this.draw();
     })
 
+    //Adds HUD element to container
     this.container.addChild(this.bar);
     this.container.addChild(this.button);
   }
 
-  draw(){
+  draw(){//Draws all HUD elements
+    //Sets width and height of HUD
     let width = app.renderer.width;
     let height = app.renderer.height/16;
 
+    //Draws elements
     this.drawButton(height);
     this.drawBar(width, height);
 
     this.container.removeChildren();
-    
   }
 
-  drawBar(width:number, height:number){
+  drawBar(width:number, height:number){//Draws bar of HUD
     this.bar.beginFill(0x434343);
     this.bar.drawRect(0,0,width, height);
   }
 
-  drawButton(height:number){
+  drawButton(height:number){//Draws next turn button of HUD
+    //Draws button
     this.button.beginFill(0x900000);
     this.button.drawStar(height/2,height/2,5,height/2);
+
+    //Turns button into button
     this.button.interactive = true;
-    this.button.on("mousedown", () => alert("ree"));
-
-    this.button.zIndex = 100;
+    this.button.on("mousedown", () => alert("Next turn!"));
   }
-
-
 }
 
 export{
