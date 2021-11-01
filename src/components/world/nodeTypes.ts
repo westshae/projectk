804d7d3cd821c846396df2e32199f83a6c424b9a
westshae <shaewest02@gmail.com>
2021-11-01 22:05:33 +1300

    Changed nodeTypes to const

--- src/components/world/nodeTypes.ts
+++ src/components/world/nodeTypes.ts
@@ -3,21 +3,21 @@ import { houseTexture } from "../util/textures"
 import { mineTexture } from "../util/textures"
 import { resourceType } from "./node"
 
-let tree:resourceType =  {
+const tree:resourceType =  {
   processingTime: 2,
   defense: 0,
   type: 0,
   sprite: Sprite.from(houseTexture),
 }
 
-let ore:resourceType = {
+const ore:resourceType = {
     processingTime: 2,
     defense: 0,
     type: 1,
     sprite: Sprite.from(mineTexture),
 }
 
-let fruit:resourceType = {
+const fruit:resourceType = {
   processingTime: 3,
   defense: 0,
   type: 2,
